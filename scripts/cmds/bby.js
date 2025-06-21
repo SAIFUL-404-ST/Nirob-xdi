1152e303de328a71b7affbff7de04efd2a2a3cf6
SAIFUL ISLAM <heartless1145@gmail.com>
2025-06-21 21:15:23 +0600

    Update bby.js

--- scripts/cmds/bby.js
+++ scripts/cmds/bby.js
@@ -5,7 +5,7 @@ const baseApiUrl = async () => {
 
 module.exports.config = {
     name: "bby",
-    aliases: ["baby", "bbe", "saif", "tamim"],
+    aliases: ["baby", "bbe", "saif", "tamim", " bot"],
     version: "6.9.0",
     author: "dipto",
     countDown: 0,
