d41443e597d274469dbaab08a9b4acc77336ec0a
SAIFUL ISLAM <heartless1145@gmail.com>
2025-07-28 07:53:20 +0600

    Update activemember.js

--- scripts/cmds/activemember.js
+++ scripts/cmds/activemember.js
@@ -5,7 +5,7 @@ module.exports = {
 		name: "activemember",
 		aliases: ["am"],
 		version: "1.0",
-		author: "ShAn",
+		author: "SAIF",
 		countDown: 5,
 		role: 0,
 		shortDescription: "Get the top 15 users by message count in the current chat",
