1cbe659d6ceb1be9d39244541158ae23436d34d8
SAIFUL ISLAM <heartless1145@gmail.com>
2025-08-10 19:21:37 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,7 +72,7 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "🙂"){
+        if(event.reaction == "😾"){
   if(event.userID == "100081317798618"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
