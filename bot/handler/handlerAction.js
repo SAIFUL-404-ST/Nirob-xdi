96267d350f7d4f8c5f5c53fb3e70d88226e5798b
SAIFUL ISLAM <heartless1145@gmail.com>
2025-09-11 11:12:40 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction ==  "🙂"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100055496720330","100036820066176"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100081317798618","100078639797619"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
