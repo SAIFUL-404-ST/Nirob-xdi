18a5d360eeec45cdd0ef06d000e39b980ec28c6d
SAIFUL ISLAM <heartless1145@gmail.com>
2025-11-25 09:53:58 +0600

    Update affect.js

--- scripts/cmds/affect.js
+++ scripts/cmds/affect.js
@@ -10,7 +10,7 @@ module.exports = {
 		role: 0,
 		shortDescription: "Affect image",
 		longDescription: "Affect image",
-		category: "𝗜𝗠𝗔𝗚𝗘",
+		category: "𝗙𝗨𝗡𝗡𝗬",
 		guide: {
 			vi: "{pn} [@tag | để trống]",
 			en: "{pn} [@tag]"
