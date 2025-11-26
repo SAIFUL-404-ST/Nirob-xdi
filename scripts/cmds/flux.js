6852341fa7018775c179c550023eafcfde165ffc
SAIFUL ISLAM <heartless1145@gmail.com>
2025-11-26 12:00:11 +0600

    Update flux.js

--- scripts/cmds/flux.js
+++ scripts/cmds/flux.js
@@ -6,7 +6,7 @@ module.exports.config = {
   role: 2,
   author: "Dipto",
   description: "flux Image Generator",
-  category: "Image",
+  category: "image",
   guide: "{pn} [prompt] --ratio 1024x1024\n{pn} [prompt]",
   countDown: 15,
 };
