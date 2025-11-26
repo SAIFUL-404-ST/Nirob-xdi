33386a61bb1b4d1ecf7dbe93493bb7892a2b1206
SAIFUL ISLAM <heartless1145@gmail.com>
2025-11-26 13:28:47 +0600

    Update 0info.js

--- scripts/cmds/0info.js
+++ scripts/cmds/0info.js
@@ -10,7 +10,7 @@ module.exports = {
     role: 0,
     shortDescription: { vi: "", en: "" },
     longDescription: { vi: "", en: "" },
-    category: "owner",
+    category: "information",
     guide: { en: "" },
     envConfig: {}
   },
