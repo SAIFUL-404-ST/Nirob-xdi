98a4eada864df89eb880befb6be9e66f499293b4
SAIFUL ISLAM <heartless1145@gmail.com>
2025-11-26 12:09:21 +0600

    Update gay.js

--- scripts/cmds/gay.js
+++ scripts/cmds/gay.js
@@ -1,116 +1,112 @@
 const DIG = require("discord-image-generation");
 const fs = require("fs-extra");
+const path = require("path");
 
 module.exports = {
-	config: {
-		name: "gay",
-		version: "1.0",
-		author: "@tas33n",
-		countDown: 1,
-		role: 0,
-		shortDescription: "find gay",
-		longDescription: "",
-		category: "funny",
-		guide: "{pn} {{[on | off]}}",
-		envConfig: {
-			deltaNext: 5
-		}
-	},
+  config: {
+    name: "gay",
+    aliases: ["rainbow", "lgbt"],
+    version: "1.0",
+    author: "Saif",
+    countDown: 3,
+    role: 0,
+    shortDescription: "Anime-style gay detection with coins",
+    category: "fun",
+  },
 
-	langs: {
-		vi: {
-			noTag: "Bạn phải tag người bạn muốn tát"
-		},
-		en: {
-			noTag: "You must tag the person you want to "
-		}
-	},
+  onStart: async function ({ api, event, args, usersData, threadsData }) {
+    try {
+      const COST = 500;
+      const sender = event.senderID;
 
-	onStart: async function ({ event, message, usersData, args, getLang }) 
-	{
+      // ==== Check balance ====
+      let user = await usersData.get(sender);
+      let balance = user.money || 0;
+      if (balance < COST) {
+        return api.sendMessage(
+          `🌸 Senpai… you need **${COST} coins** to use this!  
+💰 Your balance: ${balance} coins`,
+          event.threadID, event.messageID
+        );
+      }
 
-		let mention = Object.keys(event.mentions)
-		let uid;
+      // Deduct coins
+      await usersData.set(sender, { ...user, money: balance - COST });
+      const remaining = balance - COST;
 
-		// const img = await new DIG.Gay().getImage(url);
+      // ==== Determine target ====
+      let target;
+      let targetName;
 
+      // --- Random mode ---
+      if (["r", "rnd", "random"].includes(args[0]?.toLowerCase())) {
+        const threadInfo = await api.getThreadInfo(event.threadID);
+        let candidates = threadInfo.participantIDs.filter(id => id !== sender && id !== api.getCurrentUserID());
+        if (candidates.length === 0) return api.sendMessage("Nyaa~ No one to check! 🌈", event.threadID);
+        target = candidates[Math.floor(Math.random() * candidates.length)];
+        targetName = await usersData.getName(target);
+      } 
+      // --- Tag mode ---
+      else if (Object.keys(event.mentions)[0]) {
+        target = Object.keys(event.mentions)[0];
+        targetName = event.mentions[target];
+      } 
+      // --- Reply mode ---
+      else if (event.type === "message_reply") {
+        target = event.messageReply.senderID;
+        targetName = await usersData.getName(target);
+      } 
+      // --- Fallback to random last sender ---
+      else {
+        const threadInfo = await api.getThreadInfo(event.threadID);
+        let messages = await api.getThreadMessages(event.threadID, 50);
+        for (let msg of messages) {
+          if (msg.senderID !== sender && msg.senderID !== api.getCurrentUserID()) {
+            target = msg.senderID;
+            targetName = await usersData.getName(target);
+            break;
+          }
+        }
+        if (!target) return api.sendMessage("Nyaa~ No one to check! 🌈", event.threadID);
+      }
 
-		if(event.type == "message_reply"){
-		uid = event.messageReply.senderID
-		} else{
-			if (mention[0]){
-				uid = mention[0]
-			}else{
-				console.log(" jsjsj")
-				uid = event.senderID}
-		}
+      if (target === sender) return api.sendMessage("Ara ara… you can't check yourself baka~ (>///<)", event.threadID);
 
-let url = await usersData.getAvatarUrl(uid)
-let avt = await new DIG.Gay().getImage(url)
+      // ==== Get avatar URL ====
+      const avatarTarget = await usersData.getAvatarUrl(target);
 
+      // ==== Generate image ====
+      const img = await new DIG.Gay().getImage(avatarTarget);
 
-	// 	message.reply({
-	// 		body:"",
-	// 		attachment: await global.utils.getStreamFromURL(avt)
-	// })
-			const pathSave = `${__dirname}/tmp/gay.png`;
-	fs.writeFileSync(pathSave, Buffer.from(avt));
-		let body = "look.... i found a gay"
-		if(!mention[0]) body="Baka you gay\nforgot to reply or mention someone"
-		message.reply({body:body,
-attachment: fs.createReadStream(pathSave)
-		}, () => fs.unlinkSync(pathSave));
+      // TMP path
+      const tmpPath = path.join(__dirname, "tmp");
+      if (!fs.existsSync(tmpPath)) fs.mkdirSync(tmpPath);
+      const filePath = path.join(tmpPath, `${sender}_${target}_gay.png`);
+      fs.writeFileSync(filePath, Buffer.from(img));
 
+      // ==== Anime-style replies ====
+      const senderName = await usersData.getName(sender);
 
-	}
+      const animeReplies = [
+        `Nyaa~ ${senderName}-kun found that ${targetName} is super gay! 🌈`,
+        `${targetName}-san is sparkling rainbow! ✨ Baka~`,
+        `Sugoiii~ ${senderName}-chan detected ${targetName}-kun’s gayness! 😼`,
+        `Ara ara~ ${targetName} is shining with love! 💖`,
+        `Baka! ${senderName} just discovered ${targetName}-san’s secret! 🌸`,
+        `Nyaaa~ ${targetName}-kun is full of rainbow vibes! 🌈💫`
+      ];
+
+      const chosenReply = animeReplies[Math.floor(Math.random() * animeReplies.length)];
+
+      // ==== Send message with attachment & remaining balance ====
+      await api.sendMessage({
+        body: `${chosenReply}\n\n💸 ${COST} coins deducted!\n💳 Remaining: ${remaining} coins`,
+        attachment: fs.createReadStream(filePath)
+      }, event.threadID, () => fs.unlinkSync(filePath));
+
+    } catch (err) {
+      console.log(err);
+      return api.sendMessage("Uwuuu~ Something went wrong (>_<)💦", event.threadID);
+    }
+  }
 };
-
-
-
-
-
-
-
-
-
-// 	onStart: async function ({ message, event, usersData, threadsData, args }) {
-
-
-
-
-// 		if(event.type == "message_reply"){
-//       avt = await usersData.getAvatarUrl(event.messageReply.senderID)
-//     } else{
-//       if (!uid2){avt =  await usersData.getAvatarUrl(uid1)
-//               } else{avt = await usersData.getAvatarUrl(uid2)}}
-
-
-// 		message.reply({body:"Look.... I found a gay",
-// attachment: fs.createReadStream(pathSave)
-// 		}, () => fs.unlinkSync(pathSave));
-
-
-
-
-
-// message.send({body:"Look.... I found a gay",
-// attachment: fs.createReadStream(pathSave)
-// 		}, () => fs.unlinkSync(pathSave));
-
-// st fs = require("fs-extra");
-// let url = await usersData.getAvatarUrl(event.messageReply.senderID)
-// // const img = await new DIG.Gay().getImage(url);
-		// const pathSave = `${__dirname}/tmp/gay.png`;
-		// fs.writeFileSync(pathSave, Buffer.from(avt));
-
-// // message.send({body:"Look.... I found a gay",
-// // attachment: fs.createReadStream(pathSave)
-// // 		}, () => fs.unlinkSync(pathSave));
-
-
-// 	}
-
-
-
-
-// }
