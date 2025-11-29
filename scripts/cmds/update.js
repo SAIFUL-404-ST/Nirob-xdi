42327ba0ee83baddf508541ddd8aaf42cbf1d916
SAIFUL ISLAM <heartless1145@gmail.com>
2025-11-29 06:20:55 +0600

    Update update.js

--- scripts/cmds/update.js
+++ scripts/cmds/update.js
@@ -13,7 +13,7 @@ module.exports = {
 			en: "Check for and install updates for the chatbot.",
 			vi: "Kiểm tra và cài đặt phiên bản mới nhất của chatbot trên GitHub."
 		},
-		category: "owner",
+		category: "admin",
 		guide: {
 			en: "   {pn}",
 			vi: "   {pn}"
