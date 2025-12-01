394d63ab4bde35e7bf9be867a940806f739ff2f7
SAIFUL ISLAM <heartless1145@gmail.com>
2025-12-01 10:15:38 +0600

    Update adduser.js

--- scripts/cmds/adduser.js
+++ scripts/cmds/adduser.js
@@ -13,7 +13,7 @@ module.exports = {
 			vi: "Thêm thành viên vào box chat của bạn",
 			en: "Add user to box chat of you"
 		},
-		category: "owner",
+		category: "admin",
 		guide: {
 			en: "   {pn} [link profile | uid]"
 		}
